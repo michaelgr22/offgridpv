4fea28e52aab516a629f7fbb08ada3b50945dc80
michaelgr22 <michael.graml22@outlook.de>
2021-12-19 17:04:38 +0100

    added response for post requests

--- logserver/logserver/logserver.py
+++ logserver/logserver/logserver.py
@@ -6,30 +6,38 @@ logpath = '/logstorage'
 class LogPipeline(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print(self.path)
         if self.path == '/':
             self.path = '/index.html'
         file_to_open = open(self.path[1:]).read()
-        self.send_response(200)
-        self.end_headers()
+        self.__set_response(200)
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
     def do_POST(self):
-        content_len = int(self.headers.get('Content-Length'))
-        post_body_bytes = self.rfile.read(content_len)
-        post_body_string = str(post_body_bytes, "utf-8")
+        try:
+            content_len = int(self.headers.get('Content-Length'))
+            post_body_bytes = self.rfile.read(content_len)
+            post_body_string = str(post_body_bytes, "utf-8")
+            self.__set_response(200)
+            self.wfile.write(post_body_bytes)
+        except:
+            self.__set_response(200)
         self.write_to_file(post_body_string)
 
     def write_to_file(self, message):
-        device = self.find_device(message)
-        logstring = self.create_logstring(message)
+        device = self.__find_device(message)
+        logstring = self.__create_logstring(message)
         with open('{}/{}.log'.format(logpath, device), 'a') as f:
             f.write(logstring + '\n')
 
-    def find_device(self, message):
+    def __set_response(self, response_code):
+        self.send_response(response_code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def __find_device(self, message):
         return message.split('&')[0].split('=')[1]
 
-    def create_logstring(self, message):
+    def __create_logstring(self, message):
         items = message.split('&')[1:]
         logstring = ''
         for item in items:
