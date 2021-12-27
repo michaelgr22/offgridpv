f7b03d7fb3153ec369810399340a757e68a2cc80
michaelgr22 <michael.graml22@outlook.de>
2021-12-27 20:19:25 +0100

    added cmd parameter to pass device

--- dbpipeline/dbpipeline.py
+++ dbpipeline/dbpipeline.py
@@ -1,3 +1,5 @@
+import sys
+
 from credentials import AWS_ENDPOINT, AWS_PORT, MONGO_CONNECTION_STRING, PKCS12_PASSWORD
 
 from awsiot import AwsIot
@@ -10,8 +12,11 @@ aws_iot = AwsIot(AWS_ENDPOINT, AWS_PORT, PKCS12_PASSWORD)
 mongodb = MongoDB(MONGO_CONNECTION_STRING, 'offgridpv', 'power_measurements')
 powermeasurements_collection = PowerMeasurementsCollection(mongodb)
 
+device = sys.argv[1]
 
-batterymonintor_shadow = aws_iot.get_thing_shadow('batterymonitor')
+
+batterymonintor_shadow = aws_iot.get_thing_shadow(device)
+print(batterymonintor_shadow)
 batterymonitor_measurement = BatterymonintorMeasurement.from_shadow(
     batterymonintor_shadow)
 print(batterymonitor_measurement.to_dict())
